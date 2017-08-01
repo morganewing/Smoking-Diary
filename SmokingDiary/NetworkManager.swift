c11858ef9315c5a0b93bb4af0df5d865ee208b08
Jae Pyun <jaepyun@Jaes-MacBook-Pro.local>
2017-08-01 10:49:09 +0100

    agadfaf

--- SmokingDiary/NetworkManager.swift
+++ SmokingDiary/NetworkManager.swift
@@ -95,7 +95,7 @@ class NetworkManager {
             
             //completion(false, NetworkError.BadConnection)
             
-            //print(response)
+            print(response)
         }
     }
 }
